1ba07673205f63f1b98e4ae137c2723bb650eb5e
Rohitmallem <43582088+Rohitmallem@users.noreply.github.com>
2021-01-25 04:47:03 +0100

    Update listingdictkey.py

--- listingdictkey.py
+++ listingdictkey.py
@@ -2,8 +2,8 @@ import pickle
 
 
 try: 
-	geeky_file = open('GFG.txt', 'r') 
-	dictionary_list = pickle.load(geeky_file) 
+	cheek_file = open('Input.txt', 'r') 
+	dictionary_list = pickle.load(cheek_file) 
 	
 	for d in dictionary_list: 
 		print(d) 
